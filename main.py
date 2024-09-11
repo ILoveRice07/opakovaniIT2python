0d47a34ad6bb70611eb398f63f792aaca0b85d60
ILoveRice07 <159123610+ILoveRice07@users.noreply.github.com>
2024-09-11 09:33:32 +0000

    Verze 1

---
+++ main.py
@@ -0,0 +1,32 @@
+# 1. úkol
+jmeno = "Tomáš"
+
+# 2. úkol
+prijmeni = "Nguyen"
+
+# 3. úkol
+print(jmeno,prijmeni)
+
+# 4. úkol
+vek = float(input("Zadej svůj věk v číslech:"))
+
+# 9. úkol
+if vek == int:
+ print("Děkuji")
+else:
+ print("Zadej jen celočíselnou hodnotu!!")
+
+# 5. úkol
+print("Délka jména je",len(jmeno),"písmen.")
+print("Délka příjmení je",len(prijmeni),"písmen.")
+
+# 6. úkol
+x=6
+
+# 7. úkol
+for i in range(5):
+ x=x+3
+
+# 8. úkol
+print("Výsledek cyklu je",x,".")
+
